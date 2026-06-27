0e2397fc76e65cdf646e016a216655f6ec9278d8
iMotionSoftwareTech <imotionsoftware@hotmail.com>
2026-06-28 00:42:27 +0100

    Closes #3 - Number and Date format console app

---
+++ Chapter2/NumberAndDateFormatConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberAndDateFormatConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ahmed");
+
+            Console.WriteLine("Temperature on {0:D} is {1}°C.", DateTime.Today, 23.4);
+            Console.ReadKey();
+        }
+    }
+}
