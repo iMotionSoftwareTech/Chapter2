9f887d2dad1ccc32fa13206561d21d1be9aff203
iMotionSoftwareTech <imotionsoftware@hotmail.com>
2026-06-27 23:57:34 +0100

    #1 - Hello World Console App

---
+++ Chapter2/Chapter2/HelloConsoleApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter2
+{
+    /// <summary>
+    /// This is a simple console application that prints "Hello World!" to the console.
+    /// </summary>
+    internal class HelloConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            // The start of a block
+            Console.WriteLine("Hello World!"); // A statement
+            // The end of a block
+        }
+    }
+}
