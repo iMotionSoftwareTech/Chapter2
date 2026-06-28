c894d8fe0d30ef58260595997d1d262341b3165e
iMotionSoftwareTech <imotionsoftware@hotmail.com>
2026-06-28 03:52:37 +0100

    Closes #5 - Console App to count number of methods

---
+++ Chapter2/QuantifiedMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace QuantifiedMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loop through the assemblies that this app references
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // Load the assembly so we can read its details
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                // Declare a variable to count the number of methods
+                int methodCount = 0;
+
+                // Loop through all of the types in the assembly
+                foreach (var t in a.DefinedTypes)
+                {
+                    // Add up the counts of methods
+                    methodCount += t.GetMethods().Length;
+                }
+
+                // Output the count of types and their methods
+                Console.WriteLine("{0:0} types with {1: 0} methods in {2} assembly.", a.DefinedTypes.Count(), methodCount, r.Name);
+                Console.ReadKey();
+            }
+        }
+    }
+}
